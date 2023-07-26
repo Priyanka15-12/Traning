540be7ce4f3c1d5b394110b359ca1611b12f1663
Priyanka S <priyanka.s.interns@metamation.com>
2023-07-26 10:40:45 +0530

    Initial change is done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿Random r = new Random ();
+int n = r.Next (1, 101);
+Console.Write ("Guess the random number: ");
+for (; ; ) {
+   int p = int.Parse (Console.ReadLine ());
+   Console.WriteLine (p);
+   if (p < n) {
+      Console.Write ("Your guess is too low  ");
+   } else if (p == n) {
+      Console.Write ("You got it!");
+      break;
+   } else {
+      Console.Write ("Your guess is too high  ");
+   }
 }
