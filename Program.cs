5a5d2ca7d132c3e2360d34cb97efdc8be470ae2e
Naveen Kumar <naveenkumar@metamation.com>
2023-08-01 15:55:27 +0530

    Revert "Assignment_A02"

--- Program.cs
+++ Program.cs
@@ -1,34 +1,7 @@
-﻿internal class Program {
-   private static void Main (string[] args) {
-      int a = 1, z = 100;
-      Random r = new ();
-      int n = r.Next (a, z);
-
-      Console.WriteLine ($"GUESS RANDOM NUMBER FROM {a} TO {z+1}:");
-      //Console.WriteLine (n);
-      int attempt = 0;
-      int p = 0;
-      while (p != n) {
-         // get user input as string
-         Console.Write ("Guess number: ");    
-         // Type cast - convert string to int
-         if (int.TryParse (Console.ReadLine (), out p)) {
-            // check if non-positive and print msg
-            if (p < a || p > z) {
-               Console.WriteLine ("Enter the number within the range.");
-               continue;
-            }
-            // check lesser than 
-            if (n > p)
-               Console.WriteLine ($"Your guess is too low. Enter a number greater than {p}");
-            // check greater than 
-            else if (n < p)
-               Console.WriteLine ($"Your guess is too high. Enter a number smaller than {p}");
-            // increment attempt count 
-            attempt++;                                                                                                          
-         }
-      }
-      Console.WriteLine ($"You got the answer at attempt {attempt}!");
-
-   }
+﻿namespace Training {
+    internal class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello, World!");
+        }
+    }
 }
