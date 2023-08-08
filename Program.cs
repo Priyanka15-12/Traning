76ea4944994290fc6c8885eb76fdccfeebf8fea1
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-08 09:47:52 +0530

    T01-Number conversion game

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System;
+
+namespace Training {
+   internal class Program {
+      static void Main (string[] args) {
+         Console.Write ("Input: ");
+         int dec = int.Parse (Console.ReadLine ());
+         String hexa = dec.ToString ("X");
+         Console.WriteLine ($"HEX: {hexa}");
+         string bin = "";
+         while (dec > 0) {
+            int rem = dec % 2;
+            bin = rem + bin;
+            dec = dec / 2;
+         }Console.WriteLine ($"Binary: {bin}");
+      }
+   }
 }
