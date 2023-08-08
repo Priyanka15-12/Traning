b8c1a5224827a3140f294c9e18c0ea7c0e82aecb
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-08 14:59:46 +0530

    T01-changes were done

--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
 ﻿using System;
-
 namespace Training {
    internal class Program {
       static void Main (string[] args) {
          Console.Write ("Input: ");
-         int dec = int.Parse (Console.ReadLine ());
-         String hexa = dec.ToString ("X");
-         Console.WriteLine ($"HEX: {hexa}");
-         string bin = "";
-         while (dec > 0) {
-            int rem = dec % 2;
-            bin = rem + bin;
-            dec = dec / 2;
-         }Console.WriteLine ($"Binary: {bin}");
+         string decimal_number = Console.ReadLine ();
+         if (int.TryParse (decimal_number, out int dec)) {
+            string hexa = dec.ToString ("X");
+            Console.WriteLine ($"HEX: {hexa}");
+            string bin = "";
+            while (dec > 0) {
+               int rem = dec % 2;
+               bin = rem + bin;
+               dec/= 2;
+            }
+            Console.WriteLine ($"Binary: {bin}");
+         } else Console.WriteLine ("Enter numerical value");
       }
    }
 }
