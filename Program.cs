f42a652476fc2d15ace1224417326feca8d274d0
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-09 17:04:37 +0530

    T03 Fibonacci series

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System;
+Console.Write ("Enter input: ");
+int n = int.Parse (Console.ReadLine ());
+int a = 0, b = 1, c, i = 2;
+Console.Write ($"{a} ");
+Console.Write ($"{b} ");
+while (n > i) {
+   i++;
+   c = a + b;
+   a = b;
+   b = c;
+   Console.Write ($"{c} ");
 }
