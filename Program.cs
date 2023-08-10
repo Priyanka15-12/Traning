9039b5d4e558858d9b1af2523018fee19890613f
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-10 09:01:14 +0530

    T05- Prime number checker

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
 ﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+   internal class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter a number: ");
+         int n = int.Parse (Console.ReadLine ());
+         int i = 1, count = 1;
+         while (n >= i) {
+            i++;
+            if (n % i == 0) {
+               count++;
+            }
+         }
+         if (count == 2) {
+            Console.WriteLine ($"The given number {n} is prime number.");
+         } else Console.WriteLine ($"The given number {n} is not a prime number.");
+      }
+   }
 }
