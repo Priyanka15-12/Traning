ece22ddf838c7c97c91512a984cee7a3056c9248
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-10 09:25:06 +0530

    T06-Palindrome checker

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
 ﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+   internal class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter pharse or word: ");
+         string input_word = Console.ReadLine ();
+         string word = input_word.Replace (" ", string.Empty);
+         string rev = "";
+         int w = word.Length - 1;
+         for (int i = 0; i <= w; w--) {
+            rev = rev + word[w];
+         }
+         if (word.ToLower () == rev.ToLower ()) {
+            Console.WriteLine ("palindrome");
+         } else Console.WriteLine ("not palindrome");
+      }
+   }
 }
