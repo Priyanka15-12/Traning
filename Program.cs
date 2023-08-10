89b5c7dc51c0d710729c7bf84f6868cba0c6b530
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-10 08:55:39 +0530

    T04-LCM and GCD

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
 ﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+   internal class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter num1: ");
+         int n1 = int.Parse (Console.ReadLine ());
+         Console.Write ("Enter num2: ");
+         int n2 = int.Parse (Console.ReadLine ());
+         int a = n1, b = n2;
+         while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+         }
+         Console.WriteLine ($"GCD of {n1} and {n2} is {a}");
+         int lcm = n1 * n2 / a;
+         Console.WriteLine ($"LCM of {n1} and {n2} is {lcm}");
+      }
+   }
 }
