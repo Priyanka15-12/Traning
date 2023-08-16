9bfacc4e25f8fb34de3d0ceada7cad7c9f91893f
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-16 15:58:19 +0530

    updated

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@ Console.Write ("Enter input: ");
 int n = int.Parse (Console.ReadLine ());
 int a = 0, b = 1, i = 2;
 Console.Write ($"{a} {b} ");
-while (n > i) {
-   i++; int c = a + b;
+while (n >i++) {
+   int c = a + b;
    a = b;
    b = c;
    Console.Write ($"{c} ");
