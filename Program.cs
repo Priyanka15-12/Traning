ea04c58a849b5fb6220dbcf4b586375af64c5a66
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-16 14:51:10 +0530

    T03-Changes are done.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 Console.Write ("Enter input: ");
 int n = int.Parse (Console.ReadLine ());
-int a = 0, b = 1, c, i = 2;
-Console.Write ($"{a} ");
-Console.Write ($"{b} ");
+int a = 0, b = 1, i = 2;
+Console.Write ($"{a} {b} ");
 while (n > i) {
-   i++;
-   c = a + b;
+   i++; int c = a + b;
    a = b;
    b = c;
    Console.Write ($"{c} ");
