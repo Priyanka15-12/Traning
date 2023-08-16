a1f60caacac6ae7b7bcacdcb413d57318c964949
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-16 15:45:05 +0530

    Multiplication tables

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+   internal class Program {
+      static void Main (string[] args) {
+         //Multiplication upto n
+         Console.Write ("Enter num: ");
+         int n = int.Parse (Console.ReadLine ());
+         int i = 0;
+         while (n > i) {
+            i++;
+            int b = 3 * i;
+            Console.WriteLine ($"3 * {i,2} = {b}");
+         }
+      }
+   }
 }
