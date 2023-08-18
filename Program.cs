4c1f58b41c30a64b3d29b6494bff7da25bac11a6
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-18 14:16:45 +0530

    T09-Print a diamond using asterisk

--- Program.cs
+++ Program.cs
@@ -1,7 +1,33 @@
 ﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+   internal class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter half the number of rows: ");
+         int n = int.Parse (Console.ReadLine ());
+         for (int i = 0; i<=n; i++) {
+            for (int j = n; j > i; j--) {
+               Console.Write (" ");
+            }
+            for (int j = 0; j <= i; j++) {
+               Console.Write ("*");
+            }
+            for (int j = 0; j < i; j++) {
+               Console.Write ("*");
+            }
+            Console.WriteLine ();
+         }
+         for (int i = 1; i <= n; i++) { 
+            for (int j = 0; j < i; j++) {
+               Console.Write (" ");
+            }
+            for (int j = n; j >= i; j--) {
+               Console.Write ("*");
+            }
+            for (int j = n; j > i; j--) {
+               Console.Write ("*");
+            }
+            Console.WriteLine ();
+         }
+      }
+   }
 }
+
