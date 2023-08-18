7bbf55d6c84bfe7a54446dddedca9866ea7f13bd
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-18 16:44:44 +0530

    T12- Pascal's Triangle.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
 ﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+   internal class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter rows for Pascal's triangle: ");
+         int n = int.Parse (Console.ReadLine ());
+         for (int i = 0; i < n; i++) {
+            for (int j = n; j > i; j--) {
+               Console.Write ("  ");
+            }
+            int a = 1;
+            for (int k = 0; k <= i; k++) {
+               if (k == 0 ) {
+                  a = 1;
+               } else
+                  a = a * (i - k + 1) / k;
+              Console.Write ($"{a,-3} ");
+            }
+            Console.WriteLine ();
+         }
+      }
+   }
 }
