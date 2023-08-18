82a313998421a3e132d03c0ed81defd4360d409f
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-18 17:06:34 +0530

    T11- Digital root of a number.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
 ﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+   internal class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter a number to find digit root: ");
+         int n = int.Parse (Console.ReadLine ()); 
+         int quo, rem ;
+         while (n > 9) {
+            int add = 0;
+            while (n > 0) {
+               quo = n / 10;
+               rem = n % 10;
+               add += rem;
+               n = quo;
+            }
+            n = add;
+         }
+         Console.WriteLine ($"The digit root of given number is {n}");
+      }
+   }
 }
