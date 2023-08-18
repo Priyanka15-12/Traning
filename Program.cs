f26fb07e409b88a8ef5b7c334e7c9f42a2cb6019
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-18 14:22:16 +0530

    T10- Reverse a given number.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
 ﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+   internal class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter number: ");
+         int a = int.Parse (Console.ReadLine ());
+         int num = a;
+         int reversed_num = 0;
+         while (a > 0) {
+            int rem = a % 10;
+            reversed_num = reversed_num * 10 + rem;
+            a = a / 10;
+         }
+         Console.WriteLine ($"Reversed number {reversed_num}.");
+         if (num == reversed_num) {
+            Console.WriteLine ($"{num} is palindrome number.");
+         } else
+            Console.WriteLine ($"{num} is not a palindrome number.");
+      }
+   }
 }
