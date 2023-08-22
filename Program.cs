44175b7475863a9ce841b7c5b0548a1006f4c474
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-22 09:11:58 +0530

    Changes in digital root.

--- Program.cs
+++ Program.cs
@@ -3,18 +3,13 @@
       static void Main (string[] args) {
          Console.Write ("Enter a number to find digit root: ");
          int n = int.Parse (Console.ReadLine ()); 
-         int quo, rem ;
          while (n > 9) {
             int add = 0;
             while (n > 0) {
-               quo = n / 10;
-               rem = n % 10;
-               add += rem;
-               n = quo;
-            }
-            n = add;
-         }
-         Console.WriteLine ($"The digit root of given number is {n}");
+               add += n % 10;
+               n /= 10;
+            }n = add;
+         }Console.WriteLine ($"The digit root of given number is {n}");
       }
    }
 }
