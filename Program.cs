6b11efd5c024c0ef50b8f52924dacbf32194499e
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-22 17:09:10 +0530

    Changes in alignment - T11.

--- Program.cs
+++ Program.cs
@@ -2,14 +2,16 @@
    internal class Program {
       static void Main (string[] args) {
          Console.Write ("Enter a number to find digit root: ");
-         int n = int.Parse (Console.ReadLine ()); 
+         int n = int.Parse (Console.ReadLine ());
          while (n > 9) {
             int add = 0;
             while (n > 0) {
                add += n % 10;
                n /= 10;
-            }n = add;
-         }Console.WriteLine ($"The digit root of given number is {n}");
+            }
+            n = add;
+         }
+         Console.WriteLine ($"The digit root of given number is {n}");
       }
    }
 }
