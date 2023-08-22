b139052fbf18098a58c8f4f4ae0c9d72e1326fd4
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-22 10:08:27 +0530

    Unnecessary braces are removed.

--- Program.cs
+++ Program.cs
@@ -3,28 +3,22 @@
       static void Main (string[] args) {
          Console.Write ("Enter half the number of rows: ");
          int n = int.Parse (Console.ReadLine ());
-         for (int i = 0; i<=n; i++) {
-            for (int j = n; j > i; j--) {
+         for (int i = 0; i <= n; i++) {
+            for (int j = n; j > i; j--)
                Console.Write (" ");
-            }
-            for (int j = 0; j <= i; j++) {
+            for (int j = 0; j <= i; j++)
                Console.Write ("*");
-            }
-            for (int j = 0; j < i; j++) {
+            for (int j = 0; j < i; j++)
                Console.Write ("*");
-            }
             Console.WriteLine ();
          }
-         for (int i = 1; i <= n; i++) { 
-            for (int j = 0; j < i; j++) {
+         for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++)
                Console.Write (" ");
-            }
-            for (int j = n; j >= i; j--) {
+            for (int j = n; j >= i; j--)
                Console.Write ("*");
-            }
-            for (int j = n; j > i; j--) {
+            for (int j = n; j > i; j--)
                Console.Write ("*");
-            }
             Console.WriteLine ();
          }
       }
