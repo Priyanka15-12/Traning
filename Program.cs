ce256133aa2fae12c2551ec5d96fdbc171528531
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-24 16:49:18 +0530

    T01- Changes are done.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,28 @@
 ﻿using System;
+using System.Runtime.Intrinsics.Arm;
+
 namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.Write ("Input: ");
-         string decimal_number = Console.ReadLine ();
-         if (int.TryParse (decimal_number, out int dec) && dec!=0) {
-            string hexa = dec.ToString ("X");
-            Console.WriteLine ($"HEX: {hexa}");
-            string bin = "";
-            while (dec > 0) {
-               int rem = dec % 2;
-               bin = rem + bin;
-               dec/= 2;
+         //Convert a positive decimal number into binary and hexadecimal.
+         Console.Write ("Enter a positive number: ");
+         if ((int.TryParse (Console.ReadLine (), out int dec)) && dec>=0) {
+            int num = dec; string hex = "", bin = "";
+            if (dec == 0) Console.WriteLine ($"Hexadecimal: 0\nBinary: 0");
+            else {
+               while (dec > 0) {
+                  hex = ConvertHexa (dec % 16) + hex;
+                  dec /= 16;
+               }
+               Console.WriteLine ($"Hexadecimal: {hex}");
+               while (num > 0) {
+                  bin = num % 2 + bin;
+                  num /= 2;
+               }
+               Console.WriteLine ($"Binary: {bin}");
             }
-            Console.WriteLine ($"Binary: {bin}");
-         } else Console.WriteLine ("Enter a valid numerical value");
+         } else Console.WriteLine ("Enter a positive numerical value");
       }
+      static char ConvertHexa (int remainder) { return (remainder < 10 ? (char)('0' + remainder) : (char)('A' + remainder - 10)); }      
    }
 }
