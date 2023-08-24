9f9d26ba7cbf397bb378bfdd7bfe007f12257a17
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-24 11:17:38 +0530

    T05- Modified.

--- Program.cs
+++ Program.cs
@@ -2,17 +2,17 @@
    internal class Program {
       static void Main (string[] args) {
          Console.Write ("Enter a number: ");
-         int n = int.Parse (Console.ReadLine ());
-         int i = 1, count = 1;
-         while (n >= i) {
-            i++;
-            if (n % i == 0) {
-               count++;
+         if (int.TryParse (Console.ReadLine (), out int n)) {
+            int count = 0;
+            if (n <= 1) Console.WriteLine ($"{n} is neither composite nor prime number.");
+            else if (n == 2) Console.WriteLine ($"The given number {n} is a prime number.");
+            else if (n % 2 == 0) Console.WriteLine ($"The given number {n} is not a prime number.");
+            else if (n % 2 != 0 && n > 2) {
+               for (int i = 3; n >= i; i += 2)
+                  if (n % i == 0) count++;
+               Console.WriteLine ($"The given number {n} is{(count == 1 ? "" : " not")} a prime number");
             }
-         }
-         if (count == 2) {
-            Console.WriteLine ($"The given number {n} is prime number.");
-         } else Console.WriteLine ($"The given number {n} is not a prime number.");
+         } else Console.WriteLine ("Invalid input. Please provide a positive integer.");
       }
    }
 }
