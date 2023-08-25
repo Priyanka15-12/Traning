abdfe9152665a46d0669a8553b1560d8750a7e94
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-25 10:45:21 +0530

    Limited the number of tries.

--- Program.cs
+++ Program.cs
@@ -1,29 +1,27 @@
 using System;
+using System.Diagnostics;
+
 internal class Program {
    private static void Main (string[] args) {
       int a = 1, z = 100, attempt = 1, guess;
-      Console.Write ($"Guess random number from {a} to {z+1} and press enter key.");
-      Console.ReadKey ();
-      while (true) {
+      Console.Write ($"Guess random number from {a} to {z} and press enter key.");
+      while (attempt <= 7) {                  //find the number within 7 attempts.
          guess = (a + z) / 2;
-         char key;
          Console.Write ($"\nIs {guess} your answer?(Y/N): ");
-         key = Console.ReadKey ().KeyChar;
+         char key = Console.ReadKey ().KeyChar;
          if (key == 'Y' || key == 'y') {
-            Console.Write ($"\n{guess} is your guessed random number and I found it in {attempt} {(attempt > 1 ? "attempts" : "attempt")}.");
+            Console.Write ($"\n{guess} is your guessed number and I found it in {attempt} attempt{(attempt > 1 ? "s" : "")}.");
             break;
          } else if (key == 'N' || key == 'n') {
             attempt++;
-            Console.Write ($"\nIs that lesser than {guess}? Y/N: ");
-            key = Console.ReadKey ().KeyChar;
-            if (key == 'Y' || key == 'y')
-               z = guess - 1;
-            else if (key == 'N' || key == 'n')
-               a = guess + 1;
-            else
-               Console.Write ("\nInvalid info. Enter 'y' or 'n'.");
-         } else
-            Console.WriteLine ("\nInvalid character. Press 'Y' or 'N'.");
+            if (attempt <= 7) {
+               Console.Write ($"\nIs that lesser than {guess}? Y/N: ");
+               key = Console.ReadKey ().KeyChar;
+               if (key == 'Y' || key == 'y') z = guess;
+               else if (key == 'N' || key == 'n') a = guess + 1;
+               else Console.Write ("\nInvalid key. Enter 'Y' or 'N'.");
+            } else Console.Write ("\nI apologize for not successfully making your out-of-the-box guess!");
+         } else Console.Write ("\nInvalid key. Press 'Y' or 'N'.");
       }
    }
 }
