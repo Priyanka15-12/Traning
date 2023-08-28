145ec117c2f075c571170df4082bf061f4ac30e6
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-28 11:12:51 +0530

    T14- Reduced string.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System.Text;
+
+namespace Training {
+   internal class Program {
+      static void Main (string[] args) {
+         //Enter a string to delete a pair of same and adajacent letters.
+         Console.Write ("Enter a string: ");
+         string original = Console.ReadLine ();
+         string reduced = ReducedString (original);
+         string res = (reduced.Length > 0) ? $"Reduced string: {reduced}" : "Empty string, after reduction reduced string is empty.";
+         Console.WriteLine (res);
+      }
+
+      static string ReducedString (string original) {
+         StringBuilder reduced = new StringBuilder ();
+         int i = 0;
+         while (i < original.Length) {
+            if (i == original.Length - 1 || original[i] != original[i + 1]) {
+               reduced.Append (original[i]);
+               i++;
+            } else
+               i += 2;
+         }
+         return reduced.ToString ();
+      }
+   }
 }
