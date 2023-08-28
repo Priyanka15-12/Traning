0d891b611be9a0c9cc88aa6fcf8ef526a11b8b31
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-28 15:24:23 +0530

    T15- Isogram modified.

--- Program.cs
+++ Program.cs
@@ -1,19 +1,21 @@
-﻿namespace Training {
+﻿using System;
+
+namespace Training {
    internal class Program {
       static void Main (string[] args) {
          Console.Write ("Enter a string: ");
          string iso = Console.ReadLine ();
          bool isogram = Isogram (iso);
-         Console.WriteLine ($"{iso} is {(Isogram (iso) ?"":"not ")}an isogram.");
+         Console.WriteLine ($"{iso} is {(Isogram (iso) ? "" : "not ")}an isogram.");
       }
 
       static bool Isogram (string iso) {
          iso = iso.ToLower ();
-         for (int i = 0; i < iso.Length - 1; i++) {
-            for (int j = i + 1; j < iso.Length; j++) {
-               if (iso[i] == iso[j])
-                  return false;
-            }
+         HashSet<char> chars = new HashSet<char> ();
+         foreach (char c in iso) {
+            if (chars.Contains (c))
+               return false;
+            chars.Add (c);
          }
          return true;
       }
