34976a886a2716d34092f511defab62886f27541
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-28 13:04:13 +0530

    T15- Isogram

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
 ﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+   internal class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter a string: ");
+         string iso = Console.ReadLine ();
+         bool isogram = Isogram (iso);
+         Console.WriteLine ($"{iso} is {(Isogram (iso) ?"":"not ")}an isogram.");
+      }
+
+      static bool Isogram (string iso) {
+         iso = iso.ToLower ();
+         for (int i = 0; i < iso.Length - 1; i++) {
+            for (int j = i + 1; j < iso.Length; j++) {
+               if (iso[i] == iso[j])
+                  return false;
+            }
+         }
+         return true;
+      }
+   }
 }
