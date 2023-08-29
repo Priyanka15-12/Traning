ff0b94fdcc6834f724b2417db1cf6008ef4a2a4e
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-29 14:37:37 +0530

    T21 - Swapping of two variables.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+   internal class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter a number for a= ");
+         int a = int.Parse (Console.ReadLine ());
+         Console.Write ("Enter a number for b= ");
+         int b = int.Parse (Console.ReadLine ());
+         swap (a, b);
+         Console.Write($"After swapping,\na = {b}\nb = {a}");
+      }
+      static void swap (int x, int y) {
+         (x, y) = (y, x);
+      }
+   }
 }
