4e50085040cd05f53a7f41e6806f88cda74d37da
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-29 16:22:18 +0530

    T19 - Factorial.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
 ﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+   internal class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter a number: ");
+         int n = int.Parse (Console.ReadLine ());
+         Console.Write ($"Factorial of {n} is {factorial (n)}.");
+      }
+      static int factorial (int n) {
+         int fact = 1;
+         for (int i = 1; i < n; i++) {
+            fact *= i;
+         }
+         return fact;
+      }
+   }
 }
