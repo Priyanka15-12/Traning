16ac7d7578e4560c263c12eb0a3c0755e57d2adf
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-29 12:24:43 +0530

    T16- Abecedarian.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,33 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System;
+
+namespace Training {
+   internal class Program {
+      static void Main (string[] args) {
+         string[] words = { "glory", "floor", "ant", "victory", "aegilops", "access" };
+         wordsLength = new int[words.Length];
+         int index = 0;
+         foreach (string word in words) {
+            if (abecedarian (word, index))
+               Console.WriteLine ($"Abecedarian word :{word}");
+            index++;
+         }
+         if (maxIndex != -1)
+            Console.WriteLine ($"'{words[maxIndex]}' is the longest abecedarian word of given array.");
+         else Console.WriteLine ("No abecedarian word here.");
+      }
+      static bool abecedarian (string word, int index) {
+         for (int i = 1; i < word.Length; i++) {
+            if (word[i] < word[i - 1])
+               return false;
+         }
+         if (word.Length > maxLength) {
+            maxLength = word.Length;
+            maxIndex = index;
+         }
+         return true;
+      }
+      static int maxLength = 0;
+      static int maxIndex = -1;
+      static int[] wordsLength;
+   }
 }
