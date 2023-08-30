7d18f8a7e72b545fe6f57c35d9b1de57c534429b
Priyanka S <priyanka.s.interns@metamation.com>
2023-08-30 10:18:03 +0530

    T18 - Armstrong number.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
 ﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+   internal class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter a positive number: ");
+         int n = int.Parse (Console.ReadLine ());
+         int temp = n;
+         Console.Write ($"{n} is a {(armstrong (n) == temp ? "" : "in")}valid armstrong number.");
+      }
+      static int armstrong (int n) {
+         int sum = 0, intlen = n.ToString ().Length;
+         while (n > 0) {
+            sum += (int)Math.Pow (n % 10, intlen);
+            n /= 10;
+         }
+         return sum;
+      }
+   }
 }
