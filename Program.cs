ca444c43a43b77832d31d76786ed5d129054126e
Priyanka S <priyanka.s.interns@metamation.com>
2023-09-01 17:27:13 +0530

    T18.1 - nth Armstrong.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
 ﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+   internal class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter the value of n: ");
+         int n = int.Parse (Console.ReadLine ());
+         Console.WriteLine ($"The {n}th armstrong number is {armstrong (n)}");
+      }
+      static int armstrong (int n) {
+         int count = 0;
+         for (int i = 1; i > 0; i++) {
+            int sum = 0, num = i;
+            int length = num.ToString ().Length;
+            while (num > 0) {
+               sum += ((int)Math.Pow (num % 10, length));
+               num /= 10;
+            }
+            if (i == sum) count++;
+            if (count == n) 
+               return sum;
+         }
+         return 0;
+      }
+   }
 }
