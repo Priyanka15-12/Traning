4a5f0d75b887e07a737f160d7ae04a4fb3345fb8
Priyanka S <priyanka.s.interns@metamation.com>
2023-09-05 15:44:45 +0530

    T16 - Changes are done.

--- Program.cs
+++ Program.cs
@@ -3,23 +3,21 @@
 namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         string[] words = { "glory", "floor", "ant", "victory", "aegilops", "access" };
-         wordsLength = new int[words.Length];
+         string[] abecedarianArr = { "glory", "floor", "ant", "victory", "aegilops", "access" };
          int index = 0;
-         foreach (string word in words) {
-            if (abecedarian (word, index))
+         foreach (string word in abecedarianArr) {
+            if (Abecedarian (word, index))
                Console.WriteLine ($"Abecedarian word :{word}");
             index++;
          }
          if (maxIndex != -1)
-            Console.WriteLine ($"'{words[maxIndex]}' is the longest abecedarian word of given array.");
+            Console.WriteLine ($"'{abecedarianArr[maxIndex]}' is the longest abecedarian word of given array.");
          else Console.WriteLine ("No abecedarian word here.");
       }
-      static bool abecedarian (string word, int index) {
-         for (int i = 1; i < word.Length; i++) {
+      static bool Abecedarian (string word, int index) {
+         for (int i = 1; i < word.Length; i++)
             if (word[i] < word[i - 1])
                return false;
-         }
          if (word.Length > maxLength) {
             maxLength = word.Length;
             maxIndex = index;
@@ -28,6 +26,5 @@ namespace Training {
       }
       static int maxLength = 0;
       static int maxIndex = -1;
-      static int[] wordsLength;
    }
 }
