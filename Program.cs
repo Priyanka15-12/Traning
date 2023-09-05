fb8735ff12bbcfec500978826103b5b2c5e263ee
Priyanka S <priyanka.s.interns@metamation.com>
2023-09-05 11:09:15 +0530

    Modified code - T18

--- Program.cs
+++ Program.cs
@@ -2,14 +2,15 @@
    internal class Program {
       static void Main (string[] args) {
          Console.Write ("Enter a positive number: ");
-         int n = int.Parse (Console.ReadLine ());
-         int temp = n;
-         Console.Write ($"{n} is a {(armstrong (n) == temp ? "" : "in")}valid armstrong number.");
+         if(int.TryParse (Console.ReadLine (),out int n) && n >= 0)
+            Console.Write ($"{n} is a {(Armstrong (n) == n ? "" : "In")}valid armstrong number.");
+         else Console.WriteLine ("Invalid input. Enter a positive number.");
       }
-      static int armstrong (int n) {
-         int sum = 0, intlen = n.ToString ().Length;
+
+      static int Armstrong (int n) {
+         int sum = 0, intLen = n.ToString ().Length;
          while (n > 0) {
-            sum += (int)Math.Pow (n % 10, intlen);
+            sum += (int)Math.Pow (n % 10, intLen);
             n /= 10;
          }
          return sum;
