267602c3307cfafcdee8b636ac134bb81fe150d2
Priyanka S <priyanka.s.interns@metamation.com>
2023-09-05 12:54:43 +0530

    Changes are done - T21

--- Program.cs
+++ Program.cs
@@ -1,15 +1,13 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.Write ("Enter a number for a= ");
+         Console.Write ("Enter a number for a = ");
          int a = int.Parse (Console.ReadLine ());
-         Console.Write ("Enter a number for b= ");
+         Console.Write ("Enter a number for b = ");
          int b = int.Parse (Console.ReadLine ());
-         swap (ref a, ref b);
-         Console.Write($"After swapping,\na = {a}\nb = {b}");
-      }
-      static void swap (ref int x, ref int y) {
-         (x, y) = (y, x);
+         Action<int, int> Swap = (x, y) => (a, b) = (b, a);
+         Swap (a, b);
+         Console.Write ($"After swapping,\na = {a}\nb = {b}");
       }
    }
 }
