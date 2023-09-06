a9f83353c4f86a2f916d48aa51ad8a6b60fb4f9e
Priyanka S <priyanka.s.interns@metamation.com>
2023-09-06 14:26:09 +0530

    Modified - T14

--- Program.cs
+++ Program.cs
@@ -7,20 +7,18 @@ namespace Training {
          Console.Write ("Enter a string: ");
          string original = Console.ReadLine ();
          string reduced = ReducedString (original);
-         string res = (reduced.Length > 0) ? $"Reduced string: {reduced}" : "Empty string, after reduction reduced string is empty.";
-         Console.WriteLine (res);
+         Console.WriteLine ((reduced.Length > 0) ? $"Reduced string: {reduced}" : "Empty string, after reduction reduced string is empty.");
       }
 
       static string ReducedString (string original) {
-         StringBuilder reduced = new StringBuilder ();
+         StringBuilder reduced = new ();
          int i = 0;
-         while (i < original.Length) {
+         while (i < original.Length)
             if (i == original.Length - 1 || original[i] != original[i + 1]) {
                reduced.Append (original[i]);
                i++;
             } else
                i += 2;
-         }
          return reduced.ToString ();
       }
    }
