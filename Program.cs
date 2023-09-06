f74359177a5c65feb2c6fbe849785c7091fd81a1
Priyanka S <priyanka.s.interns@metamation.com>
2023-09-06 12:59:44 +0530

    Changes are done - T15

--- Program.cs
+++ Program.cs
@@ -5,18 +5,14 @@ namespace Training {
       static void Main (string[] args) {
          Console.Write ("Enter a string: ");
          string iso = Console.ReadLine ();
-         bool isogram = Isogram (iso);
          Console.WriteLine ($"{iso} is {(Isogram (iso) ? "" : "not ")}an isogram.");
       }
 
       static bool Isogram (string iso) {
+         HashSet<char> chars = new ();
          iso = iso.ToLower ();
-         HashSet<char> chars = new HashSet<char> ();
-         foreach (char c in iso) {
-            if (chars.Contains (c))
-               return false;
-            chars.Add (c);
-         }
+         foreach (char c in iso)
+            if (chars.Contains (c) ? false : !chars.Add (c)) return false;
          return true;
       }
    }
