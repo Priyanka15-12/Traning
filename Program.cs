b9f9b66ba2145e0ae4fea066fb4be4d53b0e1718
Priyanka S <priyanka.s.interns@metamation.com>
2023-09-06 16:30:05 +0530

    T19 - Modified

--- Program.cs
+++ Program.cs
@@ -2,16 +2,24 @@
    internal class Program {
       static void Main (string[] args) {
          Console.Write ("Enter a positive number: ");
-         if (int.TryParse (Console.ReadLine (), out int n))
+         if (int.TryParse (Console.ReadLine (), out int n) && n >= 0)
             Console.Write ($"Factorial of {n} is {Factorial (n)}.");
          else Console.WriteLine ("Invalid character. Enter a positive integer.");
       }
-      static long Factorial (int n) {
-         //works upto 20
-         long fact = 1;
+      static int Factorial (int n) {
+         //works upto 12!
+         int fact = 1;
          for (int i = 1; i <= n; i++)
             fact *= i;
          return fact;
+
       }
+      //static long Factorial (int n) {
+      //   works upto 20!
+      //   long fact = 1;
+      //   for (int i = 1; i <= n; i++)
+      //      fact *= i;
+      //   return fact;
+      //}
    }
 }
