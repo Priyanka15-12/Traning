b3cf5c4940c093d1890e88f22ce5f94353714386
Priyanka S <priyanka.s.interns@metamation.com>
2023-09-08 14:58:36 +0530

    T12 - Changes are done.

--- Program.cs
+++ Program.cs
@@ -2,11 +2,10 @@
    internal class Program {
       static void Main (string[] args) {
          Console.Write ("Enter rows for Pascal's triangle: ");
-         int n = int.Parse (Console.ReadLine ());
+         int.TryParse (Console.ReadLine (), out int n);
          for (int i = 0; i < n; i++) {
-            for (int j = n; j > i; j--) {
+            for (int j = n; j > i; j--)
                Console.Write ("  ");
-            }
             int a = 1;
             for (int k = 0; k <= i; k++) {
                a = (k == 0 ? 1 : a * (i - k + 1) / k);
