c0d5251e77944b587d7a69a0a5b89e3eaa04309d
Priyanka S <priyanka.s.interns@metamation.com>
2023-09-08 15:07:12 +0530

    T10 - Changes are done.

--- Program.cs
+++ Program.cs
@@ -2,14 +2,14 @@
    internal class Program {
       static void Main (string[] args) {
          Console.Write ("Enter number: ");
-         int a = int.Parse (Console.ReadLine ());
-         int num = a, reversed_num = 0;
+         int.TryParse (Console.ReadLine (), out int a);
+         int num = a, reversedNum = 0;
          while (a > 0) {
-            reversed_num = reversed_num * 10 + a % 10;
+            reversedNum = reversedNum * 10 + a % 10;
             a /= 10;
          }
-         string res=(num == reversed_num ? $"{num} is a palindrome number." : $"{num} is not a palindrome number.");
-         Console.WriteLine ($"Reversed number {reversed_num}.{res}");
+         string res = (num == reversedNum ? $"{num} is a palindrome number." : $"{num} is not a palindrome number.");
+         Console.WriteLine ($"Reversed number: {reversedNum}.\n{res}");
       }
    }
 }
