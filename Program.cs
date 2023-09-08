549e7259d8124509dcf84fce2c760070eff2d2a3
Priyanka S <priyanka.s.interns@metamation.com>
2023-09-08 15:24:42 +0530

    T11 - Changes are done.

--- Program.cs
+++ Program.cs
@@ -2,16 +2,18 @@
    internal class Program {
       static void Main (string[] args) {
          Console.Write ("Enter a number to find digit root: ");
-         int n = int.Parse (Console.ReadLine ());
-         while (n > 9) {
-            int add = 0;
-            while (n > 0) {
-               add += n % 10;
-               n /= 10;
+         int.TryParse (Console.ReadLine (), out int n);
+         if (n >= 0) {
+            while (n > 9) {
+               int add = 0;
+               while (n > 0) {
+                  add += n % 10;
+                  n /= 10;
+               }
+               n = add;
             }
-            n = add;
-         }
-         Console.WriteLine ($"The digit root of given number is {n}");
+            Console.WriteLine ($"The digit root of given number is {n}");
+         } else Console.WriteLine ("Enter a positive number.");
       }
    }
 }
