80c5b23b3ac7fdf12ffb67dc66bd558010cd1aa4
Priyanka S <priyanka.s.interns@metamation.com>
2023-09-13 16:24:59 +0530

    T19 - Modified code for factorial.

--- Program.cs
+++ Program.cs
@@ -3,22 +3,25 @@
       static void Main (string[] args) {
          Console.Write ("Enter a positive number: ");
          if (int.TryParse (Console.ReadLine (), out int n) && n >= 0)
-            Console.WriteLine ($"Factorial of {n} is {(n <= 2 ? (n < 2 ? 1 : 2) : Factorial (n))}.");
+            Console.WriteLine ($"Factorial of {n} is {Factorial (n)}.");
          else Console.WriteLine ("Invalid character. Enter a positive integer.");
       }
       static int Factorial (int n) {
-         //works from 3! to 12!
+         //works upto 12!
          int fact = 1;
+         if (n == 0) return 1;
+         else if (n <= 2) return n;
          for (int i = 2; i <= n; i++)
             fact *= i;
          return fact;
       }
-      //static long Factorial (int n) {
-      //   works from 3! to 20!
-      //   long fact = 1;
-      //   for (int i = 2; i <= n; i++)
-      //      fact *= i;
-      //   return fact;
-      //}
+      /*static long Factorial (int n) {
+      works upto 20!
+         long fact = 1;
+         for (int i = 2; i <= n; i++)
+            fact *= i;
+         return fact;
+      }
+      */
    }
 }
