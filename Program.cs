d0de4c2f9b23b4556b059fbd1ae7836b580ec5e2
Priyanka S <priyanka.s.interns@metamation.com>
2023-09-13 15:02:30 +0530

    T20 - Modified code for individual digits

--- Program.cs
+++ Program.cs
@@ -1,31 +1,30 @@
-﻿namespace Training {
+﻿using System;
+
+namespace Training {
    internal class Program {
       static void Main (string[] args) {
          Console.Write ("Enter decimal number: ");
          decimal.TryParse (Console.ReadLine (), out decimal dec);
-         Console.Write ("Integral Part: ");
-         DeciDigit (dec);
+         Console.Write ($"Integral Part: {(dec < 0 ? "-" : "")}");
+         Array.ForEach (GetIntArrForIntegral ((int)dec), i => Console.Write ($"{i} "));
          Console.Write ("\nFractional Part: ");
          Fraction (dec);
       }
-      static void DeciDigit (decimal d) {
-         int whole = (int)d;
-         int[] wholeArr = new int[whole.ToString ().Length];
-         for (int i = whole.ToString ().Length - 1; i >= 0; i--) {
-            wholeArr[i] = whole % 10;
-            whole /= 10;
-         }
-         Array.ForEach (wholeArr, i => Console.Write ($"{i} "));
-      }
       static void Fraction (decimal d) {
          decimal frac = (d - (int)d);
-         if (frac > 0) {
+         if (Math.Abs (frac) > 0) {
             string fracStr = frac.ToString ().Substring (2);
-            int[] fracArr = new int[fracStr.Length];
-            for (int i = 0; i < fracStr.Length; i++)
-               fracArr[i] = int.Parse (fracStr[i].ToString ());
-            Array.ForEach (fracArr, i => Console.Write ($"{i} "));
+            Array.ForEach (fracStr.ToCharArray (), i => Console.Write ($"{i} "));
          } else Console.WriteLine (0);
+      }
+      static int[] GetIntArrForIntegral (int n) {
+         n = Math.Abs (n);
+         int[] wholeArr = new int[n.ToString ().Length];
+         for (int i = n.ToString ().Length - 1; i >= 0; i--) {
+            wholeArr[i] = n % 10;
+            n /= 10;
+         }
+         return wholeArr;
       }
    }
 }
