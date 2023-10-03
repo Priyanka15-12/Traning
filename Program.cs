802f088ebe0337aa32ac50b8a7d7e65f8d09ac1d
Priyanka S <priyanka.s.interns@metamation.com>
2023-10-03 10:21:46 +0530

    Spelling Bee Changes are done

--- Program.cs
+++ Program.cs
@@ -1,37 +1,55 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
+﻿// --------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch - July 2023
+// Copyright (c) Metamation India.                                              
+// ------------------------------------------------------------------------
+// Program.cs
+// SPELLING BEE
+// char[] letters = { 'U', 'X', 'A', 'L', 'T', 'N', 'E' }
+// Your program should find all valid Spelling Bee words form the given dictionary:
+// Each word is at least 4 letters long
+// Each word uses only the 7 seed letters
+// Each word MUST use the first letter (in this case, Y)
+// Any letter can be used more than once
+// The score is then computed like this:
+// 4 letter words score 1 point
+// Longer words score as many points as there are letters
+// Any word that uses all 7 seed letters (called pangrams) gets an additional 7 bonus points
+// --------------------------------------------------------------------------------------------
+namespace Training {
+   #region Program ------------------------------------------------------------------------------
+   /// <summary>Spelling Bee</summary>
+   internal class Program {
+      #region Method ---------------------------------------------
+      /// <summary>This method shows all valid Spelling Bee words with points</summary>
+      /// <param name="args">arguments</param>
+      static void Main (string[] args) {
          string[] words = File.ReadAllLines ("C:\\Users\\priyanka.s\\Downloads\\words.txt");
          char[] letters = { 'U', 'X', 'L', 'T', 'A', 'E', 'N' };
-         int total = 0, total2 = 0, total1 = 0, index = 0; ;
-         string[] wordarray = new string[words.Length];
-         int[] point = new int[words.Length];
+         List<(int, string)> wordsList = new ();
          foreach (string word in words) {
             if (word.Length >= 4 && word.Contains (letters[0]) && word.All (letters.Contains)) {
                int marks = 0;
-               if (word.Length == 4) {
-                  marks = 1;
-                  total1 = total1 + marks;
-               } else if (word.Length > 4) {
+               if (word.Length == 4)
+                  marks += 1;
+               else if (word.Length > 4) {
                   if (letters.All (word.Contains)) {
-                     marks = word.Length + 7; Console.ForegroundColor = ConsoleColor.Green;
-                  } else
-                     marks = word.Length;
-                  total2 = total2 + marks;
+                     Console.ForegroundColor = ConsoleColor.Green;
+                     marks += word.Length + 7;
+                  } else marks += word.Length;
                }
-               point[index] = marks;
-               wordarray[index] = word;
-               index++;
+               (int, string) list = (marks, word);
+               wordsList.Add (list);
             }
          }
-         Array.Sort (point, wordarray, 0, index, Comparer<int>.Create ((a, b) => b.CompareTo (a)));
-         for (int i = 0; i < index; i++) {
-            Console.WriteLine ($"{point[i],2}. {wordarray[i]}");
+         var descendingOrderedList = wordsList.OrderByDescending (x => x.Item1).ToList ();
+         for (int i = 0; i < descendingOrderedList.Count; i++) {
+            Console.WriteLine ($"{descendingOrderedList[i].Item1,3}. {descendingOrderedList[i].Item2}");
             Console.ResetColor ();
          }
-         total = total1 + total2;
-         Console.WriteLine ("---");
-         Console.WriteLine ($"{total} =Total");
-        }
-    }
+         Console.WriteLine ("----");
+         Console.WriteLine ($"{descendingOrderedList.Sum (point => point.Item1)} total");
+      }
+      #endregion
+   }
+   #endregion
 }
