06bf1175ab1e3231d7b6f4a1ddf322e4a538a9de
Priyanka S <priyanka.s.interns@metamation.com>
2023-12-01 16:30:12 +0530

    A7: Double.Parse

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,44 +1,56 @@
+using Training;
 namespace TestProject {
    [TestClass]
    public class UnitTest1 {
       [TestMethod]
-      public void DoubleParseTest () {
-         Assert.AreEqual (Double.Parse ("667.9"), double.Parse ("667.9"));
-         Assert.AreEqual (Double.Parse ("66989"), double.Parse ("66989"));
-         Assert.AreEqual (Double.Parse ("+667.9"), double.Parse ("+667.9"));
-         Assert.AreEqual (Double.Parse ("-877.86e2"), double.Parse ("-877.86e2"));
-         Assert.AreEqual (Double.Parse ("+34.34e45"), double.Parse ("+34.34e45"));
-         Assert.AreEqual (Double.Parse ("-32.56e-34"), double.Parse ("-32.56e-34"));
-         Assert.AreEqual (Double.Parse ("-45.e+234"), double.Parse ("-45.e+234"));
-         Assert.AreEqual (Double.Parse ("+1.e-1"), double.Parse ("+1.e-1"));
-         Assert.AreEqual (Double.Parse ("0.0"), double.Parse ("0.0"));
-         Assert.AreEqual (Double.Parse ("0"), double.Parse ("0"));
-         Assert.AreEqual (Double.Parse (".023e4"), double.Parse (".023e4"));
-         Assert.AreEqual (Double.Parse ("-.03e4"), double.Parse ("-.03e4"));
-         Assert.AreEqual (Double.Parse ("+.2e+3"), double.Parse ("+.2e+3"));
-         Assert.AreEqual (Double.Parse ("0.2e3"), double.Parse ("0.2e3"));
-         Assert.AreEqual (Double.Parse ("67.e34"), double.Parse ("67.e34"));
-         Assert.AreEqual (Double.Parse ("6e34"), double.Parse ("6e34"));
-         Assert.AreEqual (Double.Parse ("6734"), double.Parse ("6734"));
-         Assert.AreEqual (Double.Parse ("67."), double.Parse ("67."));
-         Assert.ThrowsException<FormatException> (() => Double.Parse ("e34"));
-         Assert.ThrowsException<FormatException> (() => Double.Parse ("+e34"));
-         Assert.ThrowsException<FormatException> (() => Double.Parse ("-e34"));
-         Assert.ThrowsException<FormatException> (() => Double.Parse ("e+34"));
-         Assert.ThrowsException<FormatException> (() => Double.Parse ("e-34"));
-         Assert.ThrowsException<FormatException> (() => Double.Parse ("e3-4"));
-         Assert.ThrowsException<FormatException> (() => Double.Parse ("3/.e34"));
-         Assert.ThrowsException<FormatException> (() => Double.Parse ("79.+e34"));
-         Assert.ThrowsException<FormatException> (() => Double.Parse ("34+e34"));
-         Assert.ThrowsException<FormatException> (() => Double.Parse ("67.8e3.4"));
-         Assert.ThrowsException<FormatException> (() => Double.Parse ("67.8e1+4"));
-         Assert.ThrowsException<FormatException> (() => Double.Parse ("67.8e3.4"));
-         Assert.ThrowsException<FormatException> (() => Double.Parse ("67.8e3.4"));
-         Assert.ThrowsException<FormatException> (() => Double.Parse ("67e8e3.4"));
-         Assert.ThrowsException<FormatException> (() => Double.Parse ("87.6e3+"));
-         Assert.ThrowsException<FormatException> (() => Double.Parse ("67.6a34"));
-         Assert.ThrowsException<FormatException> (() => Double.Parse ("67.6.34"));
-         Assert.ThrowsException<FormatException> (() => Double.Parse (".e34"));
+      public void DoubleParseValid () {
+         Assert.AreEqual (double.Parse ("0"), MyDouble.Parse ("0"));
+         Assert.AreEqual (double.Parse ("0.0"), MyDouble.Parse ("0.0"));
+         Assert.AreEqual (double.Parse ("-8772"), MyDouble.Parse ("-8772"));
+         Assert.AreEqual (double.Parse ("-.03e4"), MyDouble.Parse ("-.03e4"));
+         Assert.AreEqual (double.Parse ("-45.e+234"), MyDouble.Parse ("-45.e+234"));
+         Assert.AreEqual (double.Parse ("-32.56e-34"), MyDouble.Parse ("-32.56E-34"));
+         Assert.AreEqual (double.Parse ("-877.86E2"), MyDouble.Parse ("-877.86e2"));
+         Assert.AreEqual (double.Parse ("+667.9"), MyDouble.Parse ("+667.9"));
+         Assert.AreEqual (double.Parse ("+34.3e4"), MyDouble.Parse ("+34.3e4"));
+         Assert.AreEqual (double.Parse ("+1.e-1"), MyDouble.Parse ("+1.e-1"));
+         Assert.AreEqual (double.Parse ("+.2e+3"), MyDouble.Parse ("+.2e+3"));
+         Assert.AreEqual (double.Parse ("667.9"), MyDouble.Parse ("667.9"));
+         Assert.AreEqual (double.Parse ("66989"), MyDouble.Parse ("66989"));
+         Assert.AreEqual (double.Parse (".023e4"), MyDouble.Parse (".023e4"));
+         Assert.AreEqual (double.Parse ("0.2e3"), MyDouble.Parse ("0.2e3"));
+         Assert.AreEqual (double.Parse ("67.e34"), MyDouble.Parse ("67.e34"));
+         Assert.AreEqual (double.Parse ("67.e+4"), MyDouble.Parse ("67.e+4"));
+         Assert.AreEqual (double.Parse ("6e3"), MyDouble.Parse ("6e3"));
+         Assert.AreEqual (double.Parse ("6734"), MyDouble.Parse ("6734"));
+         Assert.AreEqual (double.Parse (".734"), MyDouble.Parse (".734"));
+         Assert.AreEqual (double.Parse (".73e4"), MyDouble.Parse (".73e4"));
+         Assert.AreEqual (double.Parse ("67."), MyDouble.Parse ("67."));
+      }
+
+      [TestMethod]
+      public void DoubleParseInvalid () {
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("e34"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("+e34"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("-e34"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("e+34"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("e-34"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse (".e34"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("e3-4"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("4e3.4"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("3/.e34"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("34+e34"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("79.+e34"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("87.6e3+"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("67.6.34"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("67.6a34"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("67.8e3.4"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("67.8e1+4"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("67.8e3.4"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("67.8e3.4"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("67e8e3.4"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("+3+3.e-2"));
+         Assert.ThrowsException<FormatException> (() => MyDouble.Parse ("+3-3.e+2"));
       }
    }
 }
--- TrainingProj/Program.cs
+++ TrainingProj/Program.cs
@@ -6,34 +6,33 @@
 // Implement double.Parse method that takes a string and returns a double.
 // --------------------------------------------------------------------------------------------
 namespace Training {
-   #region Double ------------------------------------------------------------------------------
-   /// <summary>DoubleParse</summary>
-   internal class Double {
+   #region MyDouble ----------------------------------------------------------------------------
+   /// <summary>MyDouble</summary>
+   public class MyDouble {
       #region Methods -----------------------------------------------
-      static void Main (string[] args) { }
+      static void Main () { }
 
       /// <summary>Checks if input string is in correct format or not, to implement Double.Parse</summary>
       /// <param name="input">Input string</param>
-      /// <returns>True if it is valid string, else false</returns>
+      /// <returns>True if it is a valid string, else false</returns>
       public static bool IsValid (string input) {
-         int eIndex = input.IndexOf ('e'), dotIndex = input.IndexOf ('.'),
-            plusCount = input.Count (a => a == '+'), minusCount = input.Count (a => a == '-');
+         int eIndex = input.IndexOf ('e'), dotIndex = input.IndexOf ('.');
          if (!input.All (a => a is '.' or 'e' or '+' or '-' || char.IsDigit (a))) return false;
          if (input.StartsWith ('e') || input.EndsWith ('e')) return false;
+         if ((input.Count (a => a == 'e') > 1) || input.Count (a => a == '.') > 1) return false;
          if (input.Any (a => a == 'e')) {
-            if (!char.IsDigit (input[eIndex - 1]) && (input[eIndex - 1] != '.' && !char.IsDigit (input[eIndex - 2]))) return false;
+            if (!char.IsDigit (input[eIndex - 1]) &&
+                input[eIndex - 1] == '.' && input.StartsWith ('.')) return false;
             if (eIndex < dotIndex) return false;
          }
-         if (!isValidPosition ('+')) return false;
-         if (!isValidPosition ('-')) return false;
+         if (!IsValidPlusMinus ('+') || !IsValidPlusMinus ('-')) return false;
          return true;
 
-         /// <summary>Checks if plus or minus is in correct position according to count</summary>
+         /// <summary>Checks if plus or minus is in correct positions</summary>
          /// <param name="c">char plus or minus</param>
-         /// <returns>True if position of char is valid</returns>
-         bool isValidPosition (char c) {
-            int count = input.Count (a => a == c);
-            switch (count) {
+         /// <returns>True if positions are valid</returns>
+         bool IsValidPlusMinus (char c) {
+            switch (input.Count (a => a == c)) {
                case 2:
                   if (!(input[0] == c && input[eIndex + 1] == c)) return false; break;
                case 1:
@@ -43,35 +42,41 @@ namespace Training {
          }
       }
 
-      /// <summary>Parse the given string into double with decimal point</summary>
+      /// <summary>Parse the given string with decimal point into double</summary>
       /// <param name="input">Input string</param>
       /// <returns>Returns parsed double</returns>
-      static double ParseDecimalPart (string input) {
-         string[] inputStringArr = input.Split ('.');
-         double integralPart = int.Parse (inputStringArr[0]);
-         int length = inputStringArr[1].Length;
-         double fractionalPart = length == 0 ? 0 : (int.Parse (inputStringArr[1])) / Math.Pow (10, length);
-         if (integralPart < 0) fractionalPart = -1 * fractionalPart;
-         return integralPart + fractionalPart;
+      static double GetParseDecimal (string input) {
+         string[] inputParts = input.Split ('.');
+         double integralPart = inputParts[0].Length == 0
+                               ? 0 : inputParts[0] is "-" or "+"
+                               ? 0 : Math.Abs (int.Parse (inputParts[0]));
+         int fracLength = inputParts[1].Length;
+         double fractionalPart = fracLength == 0
+                               ? 0 : int.Parse (inputParts[1]) / Math.Pow (10, fracLength);
+         var result = integralPart + fractionalPart;
+         return input[0] == '-' ? -result : result;
       }
 
-      /// <summary>Parse the given string into double with exponent</summary>
+      /// <summary>Parse the given string with exponent into double</summary>
       /// <param name="input">Input string</param>
       /// <returns>Returns parsed double</returns>
-      static double ParseExponent (string input) {
-         string[] inputStringArr = input.Split ('e');
-         double beforeE = inputStringArr[0].Contains ('.') ? ParseDecimalPart (inputStringArr[0]) : int.Parse (inputStringArr[0]);
-         return beforeE * Math.Pow (10, int.Parse (inputStringArr[1]));
+      static double GetParseExponent (string input) {
+         string[] parts = input.Split ('e');
+         double beforeE = parts[0].Contains ('.')
+                          ? GetParseDecimal (parts[0]) : int.Parse (parts[0]);
+         return beforeE * Math.Pow (10, int.Parse (parts[1]));
       }
 
-      /// <summary>Checks if string is valid or not</summary>
-      /// <param name="input">Input string</param>
-      /// <returns>Returns parsed double, if the string is valid</returns>
-      /// <exception cref="FormatException">Throws exceptionm, when input the string is invalid</exception>
-      public static double Parse (string input) {
-         if (!IsValid (input)) throw new FormatException ($"The input string '{input}' was not in a correct format.'");
-         double doubleValue = input.Contains ('e') ? ParseExponent (input) : (input.Contains ('.') ? ParseDecimalPart (input) : int.Parse (input));
-         return doubleValue;
+      /// <summary>Parses the string into double, if valid</summary>
+      /// <param name="str">Input string</param>
+      /// <returns>Returns parsed double</returns>
+      /// <exception cref="FormatException">Throws exception, when input string is invalid</exception>
+      public static double Parse (string str) {
+         string input = str.ToLower ();
+         if (!IsValid (input))
+            throw new FormatException ($"The input string '{input}' was not in a correct format.'");
+         return input.Contains ('e') ? GetParseExponent (input) : (input.Contains ('.')
+                                     ? GetParseDecimal (input) : int.Parse (input));
       }
       #endregion
    }
