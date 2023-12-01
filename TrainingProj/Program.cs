d8f83ebef39435e2685555ce399c0e5bd7d5164d
Priyanka S <priyanka.s.interns@metamation.com>
2023-12-01 11:13:07 +0530

    A7: Double.Parse

--- TrainingProj/Program.cs
+++ TrainingProj/Program.cs
@@ -24,25 +24,29 @@ namespace Training {
             if (!char.IsDigit (input[eIndex - 1]) && (input[eIndex - 1] != '.' && !char.IsDigit (input[eIndex - 2]))) return false;
             if (eIndex < dotIndex) return false;
          }
-         switch (plusCount) {
-            case 2:
-               if (!(input[0] == '+' && input[eIndex + 1] == '+')) return false; break;
-            case 1:
-               if (!(input[0] == '+' || input[eIndex + 1] == '+')) return false; break;
-         }
-         switch (minusCount) {
-            case 2:
-               if (!(input[0] == '-' && input[eIndex + 1] == '-')) return false; break;
-            case 1:
-               if (!(input[0] == '-' || input[eIndex + 1] == '-')) return false; break;
-         }
+         if (!isValidPosition ('+')) return false;
+         if (!isValidPosition ('-')) return false;
          return true;
+
+         /// <summary>Checks if plus or minus is in correct position according to count</summary>
+         /// <param name="c">char plus or minus</param>
+         /// <returns>True if position of char is valid</returns>
+         bool isValidPosition (char c) {
+            int count = input.Count (a => a == c);
+            switch (count) {
+               case 2:
+                  if (!(input[0] == c && input[eIndex + 1] == c)) return false; break;
+               case 1:
+                  if (!(input[0] == c || input[eIndex + 1] == c)) return false; break;
+            }
+            return true;
+         }
       }
 
       /// <summary>Parse the given string into double with decimal point</summary>
       /// <param name="input">Input string</param>
       /// <returns>Returns parsed double</returns>
-      static double Decimal (string input) {
+      static double ParseDecimalPart (string input) {
          string[] inputStringArr = input.Split ('.');
          double integralPart = int.Parse (inputStringArr[0]);
          int length = inputStringArr[1].Length;
@@ -54,9 +58,9 @@ namespace Training {
       /// <summary>Parse the given string into double with exponent</summary>
       /// <param name="input">Input string</param>
       /// <returns>Returns parsed double</returns>
-      static double Exponent (string input) {
+      static double ParseExponent (string input) {
          string[] inputStringArr = input.Split ('e');
-         double beforeE = inputStringArr[0].Contains ('.') ? Decimal (inputStringArr[0]) : int.Parse (inputStringArr[0]);
+         double beforeE = inputStringArr[0].Contains ('.') ? ParseDecimalPart (inputStringArr[0]) : int.Parse (inputStringArr[0]);
          return beforeE * Math.Pow (10, int.Parse (inputStringArr[1]));
       }
 
@@ -66,7 +70,7 @@ namespace Training {
       /// <exception cref="FormatException">Throws exceptionm, when input the string is invalid</exception>
       public static double Parse (string input) {
          if (!IsValid (input)) throw new FormatException ($"The input string '{input}' was not in a correct format.'");
-         double doubleValue = input.Contains ('e') ? Exponent (input) : (input.Contains ('.') ? Decimal (input) : int.Parse (input));
+         double doubleValue = input.Contains ('e') ? ParseExponent (input) : (input.Contains ('.') ? ParseDecimalPart (input) : int.Parse (input));
          return doubleValue;
       }
       #endregion
