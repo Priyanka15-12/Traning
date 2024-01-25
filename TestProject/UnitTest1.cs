bae80c4a7dfdbd77098883e28c0db3890ae20a9d
Priyanka S <priyanka.s.interns@metamation.com>
2024-01-25 17:43:28 +0530

    A10.1: Review fix

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -5,25 +5,26 @@ public class UnitTest1 {
 
    [TestMethod]
    public void FileParseTest () {
-      Assert.AreEqual (("C", "PROGRAM", "DATA", "EXT"), Program.GetParsedFilePath ("C:\\PROGRAM\\DATA.EXT"));
-      Assert.AreEqual (("W", "TRAINING", "PROGRAM", "CS"), Program.GetParsedFilePath ("W:\\TRAINING\\PROGRAM.CS"));
-      Assert.AreEqual (("C", "USERS\\DOWNLOADS", "ASSIGNMENT", "DOCX"), Program.GetParsedFilePath ("C:\\USERS\\DOWNLOADS\\ASSIGNMENT.DOCX"));
-      Assert.AreEqual (("C", "PROGRAM", "DATA", "EXT"), Program.GetParsedFilePath ("C:/PROGRAM/DATA.EXT"));
-      Assert.AreEqual (("W", "TRAINING", "PROGRAM", "CS"), Program.GetParsedFilePath ("W:/TRAINING\\PROGRAM.CS"));
-      Assert.AreEqual (("W", "TRAINING", "PROGRAM", "CS"), Program.GetParsedFilePath ("W:\\TRAINING/PROGRAM.CS"));
+      Assert.AreEqual ("C", Program.GetParsedFilePath ("C:\\PROGRAM\\DATA.EXT").drive);
+      Assert.AreEqual ("PROGRAM", Program.GetParsedFilePath ("C:\\PROGRAM\\DATA.EXT").folder);
+      Assert.AreEqual ("DATA", Program.GetParsedFilePath ("C:\\PROGRAM\\DATA.EXT").file);
+      Assert.AreEqual ("EXT", Program.GetParsedFilePath ("C:\\PROGRAM\\DATA.EXT").ext);
+      Assert.IsTrue (Program.CheckFilePath ("C:\\PROGRAM\\DATA.EXT"));
+      Assert.IsTrue (Program.CheckFilePath ("W:\\TRAINING\\PROGRAM.CS"));
+      Assert.IsTrue (Program.CheckFilePath ("W:/TRAINING\\PROGRAM.CS"));
+      Assert.IsTrue (Program.CheckFilePath ("W:\\TRAINING/PROGRAM.CS"));
+      Assert.AreEqual ((true, "C", "USERS\\DOWNLOADS", "ASSIGNMENT", "DOCX"),
+         Program.GetParsedFilePath ("C:\\USERS\\DOWNLOADS\\ASSIGNMENT.DOCX"));
+      Assert.AreEqual ((true, "C", "PROGRAM", "DATA", "EXT"), Program.GetParsedFilePath ("C:/PROGRAM/DATA.EXT"));
    }
 
    [TestMethod]
    public void InValidFileParser () {
-      Assert.ThrowsException<InValidPathException> (() => Program.GetParsedFilePath ("C:\\DATA.EXT"));
-      Assert.ThrowsException<InValidPathException> (() => Program.GetParsedFilePath ("W:\\TRAINING\\.CS"));
-      Assert.ThrowsException<InValidPathException> (() => Program.GetParsedFilePath ("W:\\TRAINING\\PROGRAM"));
-      Assert.ThrowsException<InValidPathException> (() => Program.GetParsedFilePath ("W:\\TRAINING\\CS"));
-      Assert.ThrowsException<InValidPathException> (() => Program.GetParsedFilePath ("WW:\\TRAINING\\PROGRAM"));
-      Assert.ThrowsException<InValidPathException> (() => Program.GetParsedFilePath ("W:\\\\TRAINING\\PROGRAM"));
-      Assert.ThrowsException<InValidPathException> (() => Program.GetParsedFilePath ("W:\\PROGRAM\\"));
-      Assert.ThrowsException<InValidPathException> (() => Program.GetParsedFilePath (":\\PROGRAM\\DATA.CS"));
-      Assert.ThrowsException<InValidPathException> (() => Program.GetParsedFilePath ("W\\PROGRAM\\DATA.CS"));
-      Assert.ThrowsException<InValidPathException> (() => Program.GetParsedFilePath ("W:PROGRAM.CS"));
+      string[] filePaths = { "C:\\DATA.EXT", "W:\\TRAINING\\.CS", "W:\\TRAINING\\PROGRAM" ,
+         "W:\\TRAINING\\CS" ,"WW:\\TRAINING\\PROGRAM", "W:\\\\TRAINING\\PROGRAM",
+         "W:\\PROGRAM\\", ":\\PROGRAM\\DATA.CS", "W\\PROGRAM\\DATA.CS",
+         "\\W:PROGRAM.CS", "W:\\PROGRAM\\DATA.CS~" };
+      foreach (var filePath in filePaths)
+         Assert.IsFalse (Program.CheckFilePath (filePath));
    }
 }
--- Training/Program.cs
+++ Training/Program.cs
@@ -17,60 +17,54 @@ namespace Training {
       /// <summary>Gets user input as a filepath and parse it</summary>
       static void Main () {
          for (; ; ) {
-            Console.Write ("File name: ");
+            Console.Write ("File Path: ");
             string fileName = Console.ReadLine ();
             if (fileName == "") break; // Exits when no input is provided.
-            try {
-               (string drive, string folder, string file, string ext) = GetParsedFilePath (fileName);
-               Console.Write ($"Drive     : {drive}\n" +
-                              $"Folder    : {folder}\n" +
-                              $"File      : {file}\n" +
+
+            (bool isValid, string drive, string folder, string file, string ext) = GetParsedFilePath (fileName);
+            if (isValid) {
+               Console.Write ($"Drive     : {drive}\r\n" +
+                              $"Folder    : {folder}\r\n" +
+                              $"Filename  : {file}\r\n" +
                               $"Extension : {ext}\n\n");
-            } catch (InValidPathException e) { Console.Write (e.Message + "\n\n"); }
+            }
+            Console.WriteLine ("Invalid File path");
          }
       }
 
       /// <summary>Parse the file path</summary>
       /// <param name="filePath">String file path</param>
-      /// <returns>Returns drive, folder, filename and extension as tuple of four string</returns>
-      public static (string, string, string, string) GetParsedFilePath (string filePath) {
+      /// <returns>Returns bool for path is valid or not, drive, folder, 
+      /// filename and extension as tuple of four string</returns>
+      public static (bool isValid, string drive, string folder, string file, string ext) GetParsedFilePath (string filePath) {
          filePath = (filePath + '~').Replace ('/', '\\');
          EState s = A;
-         int firstSlash = filePath.IndexOf ('\\'),
-             lastSlash = filePath.LastIndexOf ("\\"), dotIndex = filePath.IndexOf ('.');
          Action none = () => { }, todo;
          string drive = "", folder = "", file = "", ext = "";
          foreach (var ch in filePath) {
             (s, todo) = (s, ch) switch {
-               (A, >= 'A' and <= 'Z') => (B, none),
-               (B, ':') => (C, () => drive = filePath[0].ToString ()),
+               (A, >= 'A' and <= 'Z') => (B, () => drive = ch.ToString ()),
+               (B, ':') => (C, none),
                (C, '\\') => (D, none),
-               (D or E, >= 'A' and <= 'Z') => (E, none),
-               (E or G, '\\') => (F, () => folder = filePath[(firstSlash + 1)..lastSlash]),
-               (F or G, >= 'A' and <= 'Z') => (G, none),
-               (G, '.') => (H, () => file = filePath[(lastSlash + 1)..dotIndex]),
-               (H or I, >= 'A' and <= 'Z') => (I, none),
-               (I, '~') => (J, () => ext = filePath[(dotIndex + 1)..(filePath.Length - 1)]),
+               (D or E, >= 'A' and <= 'Z') => (E, () => folder += ch),
+               (E or G, '\\') => (F, () => { file += ch; (folder, file) = (folder + file, ""); }),
+               (F or G, >= 'A' and <= 'Z') => (G, () => file += ch),
+               (G, '.') => (H, none),
+               (H or I, >= 'A' and <= 'Z') => (I, () => ext += ch),
+               (I, '~') => (J, none),
                _ => (Z, none)
             };
             todo ();
          }
-         return s == J ? (drive, folder, file, ext) : throw new InValidPathException ("Invalid file path!");
+         return s == J ? (true, drive, folder.Remove (folder.Length - 1), file, ext) : (false, "", "", "", "");
       }
+
+      public static bool CheckFilePath (string filePath) => GetParsedFilePath (filePath).isValid;
       #endregion
    }
    #endregion
 }
 
-/// <summary>Class for invalid file path exception</summary>
-#region InValidPathException -------------------------------------------------------------------  
-public class InValidPathException : Exception {
-   public InValidPathException (string message) : base (message) { }
-}
-#endregion
-
-/// <summary>States of file path</summary>
+/// <summary>States of the file path</summary>
+/// State Transition diagram: file://W://Traning//FileParser.jpg
 public enum EState { A, B, C, D, E, F, G, H, I, J, Z };
-
-///<summary>Have a look at the State Transition diagram</summary>
-///See file://W://Traning//FileParser.jpg
